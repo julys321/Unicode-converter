e63e6644e5436c00e86880731e077f5eb315fc03
Julius Lungys <juliuslungys@gmail.com>
2018-09-28 19:09:32 +0300

    add display of result in main

--- Unicode-converter/main.cpp
+++ Unicode-converter/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include "UnicodeConverter.h"
 int main() {
 	int userInput;
+	UnicodeConverter unicodeConverter;
 	//requirements for part one
 	std::cout << "Enter integer that will be converted to UTF-8 encoding:\n";
 	std::cin >> userInput;
 	std::cout << "Result is:\n";
-	//std::cout << "U+" << _itoa_s(userInput)
+	std::cout << "U+" << unicodeConverter.convertToHexadecimal(userInput) << std::endl;
+	std::cout << unicodeConverter.getEncodedValue(userInput) << std::endl;
+	system("pause");
 	return 0;
 }
